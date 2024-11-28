8561b51294b9f7f243842246ecfcdb35f0829a96
TALITA ANTUNES <127903101+Talitaav@users.noreply.github.com>
2024-11-28 19:54:16 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import ListaDeContatos from './components/listaDeContatos'
+
+function App() {
+
+
+  return (
+    <>
+      <ListaDeContatos />
+    </>
+  )
+}
+
+export default App
